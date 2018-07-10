89b510b90009d510f2fb2e6b0386cd21cd22be5a
Marco Terrinoni <marco.terrinoni@spindox.it>
2018-07-10 21:01:33 +0200

    Minor chainge in main class indentation

--- src/main/java/it/spindox/mock/workflowmanager/WorkflowManagerApplication.java
+++ src/main/java/it/spindox/mock/workflowmanager/WorkflowManagerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkflowManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkflowManagerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WorkflowManagerApplication.class, args);
+  }
 }
